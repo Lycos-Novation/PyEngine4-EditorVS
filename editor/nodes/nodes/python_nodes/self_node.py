a58f1698ac6620a31f04d0e28d65a93dc757e82b
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2021-06-02 14:54:22 +0200

    Add SelfNode

---
+++ editor/nodes/nodes/python_nodes/self_node.py
@@ -0,0 +1,11 @@
+from editor.nodes.core import BaseNode
+
+
+class SelfNode(BaseNode):
+    __identifier__ = "Python"
+
+    NODE_NAME = "Self"
+
+    def __init__(self):
+        super().__init__()
+        self.add_output("Self")
