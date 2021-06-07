ebfef7abd43195e24ae86903191df29d558b6e40
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2021-06-07 14:47:45 +0200

    Create NumberNode

---
+++ editor/nodes/nodes/python_nodes/number_node.py
@@ -0,0 +1,12 @@
+from editor.nodes.core import BaseNode
+
+
+class NumberNode(BaseNode):
+    __identifier__ = "Python"
+
+    NODE_NAME = "Number"
+
+    def __init__(self):
+        super().__init__()
+        self.add_text_input("Number")
+        self.add_output("")
